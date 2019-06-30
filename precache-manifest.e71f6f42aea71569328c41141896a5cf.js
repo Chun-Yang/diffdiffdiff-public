27c93be8268d5dcc0b4a5096c409655a0993c4aa
yang2007chun <yang2007chun@fb.com>
2019-06-30 08:12:18 -0700

    Updates

---
+++ precache-manifest.e71f6f42aea71569328c41141896a5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5ffdba623217448dd9e88c48addada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4891ced35c2bc41ec4b",
+    "url": "/static/css/2.94883de6.chunk.css"
+  },
+  {
+    "revision": "1b23b16aaa23ba385008",
+    "url": "/static/css/main.f91874aa.chunk.css"
+  },
+  {
+    "revision": "b4891ced35c2bc41ec4b",
+    "url": "/static/js/2.88b352a2.chunk.js"
+  },
+  {
+    "revision": "1b23b16aaa23ba385008",
+    "url": "/static/js/main.4b5e0759.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
