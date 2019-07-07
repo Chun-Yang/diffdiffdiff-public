9e77c05475cba32657e8e58fe241b92b9931cf88
yang2007chun <yang2007chun@fb.com>
2019-07-07 11:43:06 -0700

    Updates

--- precache-manifest.5a2f17b41132cb57db30fd0bc61445fa.js
+++ precache-manifest.5a2f17b41132cb57db30fd0bc61445fa.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb2aa0e8cf236a825022c8221f4cc77d",
+    "revision": "5572a8f49421b7269b2f762a54bc5f0d",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.94883de6.chunk.css"
   },
   {
-    "revision": "91951218e6cdd74bc4ee",
-    "url": "/static/css/main.b47d222b.chunk.css"
+    "revision": "91a4368c6d4f30c1c90a",
+    "url": "/static/css/main.3056dc8a.chunk.css"
   },
   {
     "revision": "238e50fe01a6f4de5b94",
     "url": "/static/js/2.94f7d054.chunk.js"
   },
   {
-    "revision": "91951218e6cdd74bc4ee",
-    "url": "/static/js/main.85d98629.chunk.js"
+    "revision": "91a4368c6d4f30c1c90a",
+    "url": "/static/js/main.673966d7.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
