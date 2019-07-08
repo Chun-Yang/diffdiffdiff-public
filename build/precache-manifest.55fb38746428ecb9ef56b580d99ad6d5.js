dd377f1eb384011aa44695bc75d79b11e61540f5
yang2007chun <yang2007chun@fb.com>
2019-07-07 17:18:37 -0700

    deploy

--- build/precache-manifest.55fb38746428ecb9ef56b580d99ad6d5.js
+++ build/precache-manifest.55fb38746428ecb9ef56b580d99ad6d5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38302de2aad84d974fe1c2a3a18273b4",
+    "revision": "8db5344a5eb475b2d3accff1fe39ef62",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.94883de6.chunk.css"
   },
   {
-    "revision": "54289ed941aa11c622fe",
-    "url": "/static/css/main.74ce0989.chunk.css"
+    "revision": "de4c1be5f9ca55e14074",
+    "url": "/static/css/main.df9ff506.chunk.css"
   },
   {
     "revision": "238e50fe01a6f4de5b94",
     "url": "/static/js/2.94f7d054.chunk.js"
   },
   {
-    "revision": "54289ed941aa11c622fe",
-    "url": "/static/js/main.04941ed4.chunk.js"
+    "revision": "de4c1be5f9ca55e14074",
+    "url": "/static/js/main.2361ee90.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
